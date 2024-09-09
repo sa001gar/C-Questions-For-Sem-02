c25accbf36a27369168bb98e87a6452cb17aea77
sa001gar <kundusagar233@gmail.com>
2024-09-09 22:43:36 +0530

    Update

--- 1st.c
+++ 1st.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+// WAP to print the sum and product of digits of an integer.
+int sum(int num);
+int prod(int num);
 
 int main(void)
 {
-    printf("Sagar Ku");
-
+    int num;
+    printf("Enter the Number : "); 
+    scanf("%d",&num);
+    printf("%d",sum(num));
 
     return 0;
+}
+
+int sum(int num){
+  int total=0;
+  while(num!=0){
+	total+=num%10;
+	num/=10;
+	}
+	return total;
+
 }
